2b1c1152ab398f665a5cb38fcd4e476b757dedac
muneer-khan <muneer.ahmed@nextgeni.com>
2023-05-13 16:56:00 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { Fragment, useState } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    return setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    return setCartIsShown(false);
+  };
+
+  return (
+    <Fragment>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
